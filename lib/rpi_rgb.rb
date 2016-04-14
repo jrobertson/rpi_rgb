fcd907e7b738032b066b3426b6b03c07a02cef15
Your Name <you@example.com>
2016-04-14 13:29:12 +0100

    1st commit

---
+++ lib/rpi_rgb.rb
@@ -0,0 +1,95 @@
+#!/usr/bin/env ruby
+
+# file: rpi_rgb.rb
+
+require 'rpi_led'
+
+
+# Example
+
+=begin
+
+rgb =  RPiRGB.new(%w(4 17 27), presets: {purple: [0,100,100]})
+rgb.color = 'white'
+sleep 2
+rgb.color = 'purple'
+sleep 2
+rgb.color = 'green'
+
+=end
+
+class RPiRGB
+
+  attr_accessor :brightness
+  attr_reader :red, :green, :blue
+
+  def initialize(pins, brightness: 100, smooth: false, presets: nil)
+    
+    @red, @green, @blue = pins.map do |x| 
+      RPiLed.new(x, brightness: brightness, smooth: smooth)
+    end
+    
+    @presets = presets
+
+  end
+
+  def brightness=(val)
+    
+    @brightness = val
+    [@red, @green, @blue].each do |led|
+      b = led.brightness
+      led.brightness = val if b > 0
+    end
+    
+  end
+
+  def colour=(val)
+    
+    @colour = val
+
+    case val
+
+    when 'red'
+      mix 100, 0, 0
+
+    when 'green'
+      mix 0, 100, 0
+
+    when 'blue'
+      mix 0, 0, 100
+
+    when 'white'
+      mix 100, 100, 100
+
+    else
+      
+      return unless @presets
+
+      preset = @presets[val.to_sym]
+
+      mix(preset) if preset
+
+    end
+  end
+  
+  def colour()
+    @colour
+  end
+
+  alias color= colour=
+  alias color colour
+  
+  def mix(*values)
+    
+    values.flatten!
+    
+    [@red, @green, @blue].each(&:on)
+    red.brightness, green.brightness, blue.brightness = values
+  end
+  
+  def off()
+    [@red, @green, @blue].each(&:off)
+  end
+  
+
+end
---
+++ rpi_rgb.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name = 'rpi_rgb'
+  s.version = '0.1.0'
+  s.summary = 'Changes the colour of an RGB LED connected via GPIO to a Raspberry Pi '
+  s.authors = ['James Robertson']
+  s.files = Dir['lib/rpi_rgb.rb']
+  s.add_runtime_dependency('rpi_led', '~> 0.1', '>=0.1.2') 
+  s.signing_key = '../privatekeys/rpi_rgb.pem'
+  s.cert_chain  = ['gem-public_cert.pem']
+  s.license = 'MIT'
+  s.email = 'james@r0bertson.co.uk'
+  s.homepage = 'https://github.com/jrobertson/rpi_rgb'
+end
